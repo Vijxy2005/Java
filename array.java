cdac5576ae5c3f32acce57f4ecf6767b430fb808
Vijxy2005 <vijuram270@gmail.com>
2025-03-14 14:21:44 +0530

    up

---
+++ array.java
@@ -0,0 +1,7 @@
+public class array{
+    public static void main(String[] args) {
+        String[ ]fruits={"apple","mango"};
+        System.out.println(fruits[1]);
+        System.out.println(fruits.length);
+    }
+}
