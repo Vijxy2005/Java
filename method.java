586a2e9a14f04c37c2e34f3fd567ddc960b29ed3
Vijxy2005 <vijuram270@gmail.com>
2025-03-14 14:21:23 +0530

    up

---
+++ method.java
@@ -0,0 +1,11 @@
+public class method{
+    public static void mymethod() {
+        int a=2;
+        int b=4;
+
+        System.out.println(a*b);
+    }
+    public static void main(String[] args) {
+        mymethod();
+    }
+}
