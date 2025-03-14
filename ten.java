dc6ac3a6b71a97e0e436f27a470091f40a11f974
Vijxy2005 <vijuram270@gmail.com>
2025-03-14 11:56:57 +0530

    up

---
+++ ten.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ten {
+    public static void main(String[] args) {
+        int i=1,n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of n:"); 
+        n = scanner.nextInt();
+         System.out.println("---------------------------------------------------------------------");
+         System.out.println("your output is:");
+        while(i<=5) {
+            System.out.println(n);  
+            i++;  
+    }
+}
+}
