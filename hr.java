4a8395a4d00d18b84339ba9ac5ee63c9c58406ce
Vijxy2005 <vijuram270@gmail.com>
2025-03-14 11:36:21 +0530

    up

---
+++ hr.java
@@ -0,0 +1,10 @@
+public class hr{
+    public static void main(String[] args) {
+        int i=1,n=5;
+        do{
+            System.out.println(n);
+            i++;
+        }
+        while(i<=10);
+    }
+}
