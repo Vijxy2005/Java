bbb95bd4cdeba4e97c07bc24c4f952a9347d292f
Vijxy2005 <vijuram270@gmail.com>
2025-03-14 11:41:20 +0530

    up

---
+++ arithmetic.java
@@ -0,0 +1,14 @@
+public class arithmetic{
+    public static void main(String[]args){
+         int a=10;
+         int b=14;
+         int c=a+b;
+         int d=b-a;
+         int f=a*b;
+         int e=b/a;
+         System.out.println("Addition:"+c);
+         System.out.println("Substraction:"+d);
+         System.out.println("Multiplication:"+f);
+         System.out.println("Division:"+e);
+    }
+}
