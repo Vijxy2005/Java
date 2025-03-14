c208f0ce4f829286d861c677bb4a636d08a19d89
Vijxy2005 <vijuram270@gmail.com>
2025-03-14 11:58:23 +0530

    up

---
+++ vowel2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class vowel2 {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("How many times do you want to perform this action?");
+        n = scanner.nextInt(); 
+        for (int i=0;i<n;i++) {
+            System.out.print("Enter a character: ");
+            char input = scanner.next().charAt(0); 
+            if (isVowel(input)) {
+                System.out.println("It is a vowel.");
+            } else {
+                System.out.println("It is not a vowel.");
+            }
+        }   
+        scanner.close(); 
+    }
+    public static boolean isVowel(char c) {
+        c = Character.toLowerCase(c); 
+        return (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'); 
+    }
+}
