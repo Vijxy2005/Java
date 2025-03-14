f50dda7ad67f883c3ad1a20b60329d09ecdf5423
Vijxy2005 <vijuram270@gmail.com>
2025-03-14 11:43:11 +0530

    up

---
+++ comparison.java
@@ -0,0 +1,10 @@
+public class comparison{
+    public static void main(String[]args){
+        String n1="Santhosh";
+        String n2="Fake";
+        int a=15;
+        int b=10;
+        System.out.println(n1==n2);
+        System.out.println(a>b);
+    }
+}
