8c0345424eacbf96e23cdfff63b2552d0542764a
Vijxy2005 <vijuram270@gmail.com>
2025-03-14 11:33:29 +0530

    up

---
+++ fr.java
@@ -0,0 +1,10 @@
+public class fr {
+    public static void main(String[] args) {
+        int i;
+        String na="VJ";
+        for(i=0;i<=5;i++)
+        {
+            System.out.println(na);
+        }
+    }
+}
