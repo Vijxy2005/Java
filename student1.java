934e25e9209e80aee20ec6078e04dcc503d0b8a8
Vijxy2005 <vijuram270@gmail.com>
2025-03-14 11:54:32 +0530

    up

---
+++ student1.java
@@ -0,0 +1,10 @@
+public class student1{
+    public static void main(String[]args){
+        String name="VJR";
+        char grade='A';
+        float  percentage=85.8f;
+        System.out.println("my name is:"+name);
+        System.out.println("my rank is:"+grade);
+        System.out.println("my percentage is:"+percentage);
+    }
+}
